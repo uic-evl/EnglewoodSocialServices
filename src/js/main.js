77da556072267d8052b3f023a0544b6b000cdde4
Andrew Burks <andrewtburks@gmail.com>
2017-05-15 08:56:45 -0500

    Fixed problem with some census blocks not being shown within the neighborhood. Also changed colors to make less ugly

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,8 @@ var App = App || {};
     App.map = L.map('map').setView([41.779786, -87.644778], 15);
 
     // create the map layer using data from openstreetmap
-    // var osmUrl='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
-    var osmUrl = 'http://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
+    var osmUrl= 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    // var osmUrl = 'http://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'
   	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 
   	var osm = new L.TileLayer(osmUrl, {minZoom: 11, maxZoom: 18, attribution: osmAttrib});
@@ -20,6 +20,7 @@ var App = App || {};
 
     d3.queue()
       .defer(d3.json, "./data/ChicagoCensusBlockBoundaries.geojson")
+      .defer(d3.json, "./data/ChicagoCensusTractBoundaries.geojson")
       .defer(d3.csv, "./data/ChicagoCensusPopulations.csv")
       .defer(d3.json, "./data/CommunityAreaBoundaries.geojson")
       // .defer(d3.json, "./data/EnglewoodColumnHeaders.geojson")
@@ -27,7 +28,7 @@ var App = App || {};
       .await(dataLoaded)
   };
 
-  function dataLoaded(err, censusBoundaries, censusPopulations, communityAreas, englewoodLocations) {
+  function dataLoaded(err, blockBoundaries, tractBoundaries, censusPopulations, communityAreas, englewoodLocations) {
     if (err) throw Error(err);
 
     // store data that will need to be accessed again
@@ -40,23 +41,34 @@ var App = App || {};
       App.data.populations[block["CENSUS BLOCK"]] = block["TOTAL POPULATION"];
     }
 
+    // create a lookup to convert a census tract to a community area
+    App.data.tractInfo = {};
+
+    for (let tract of tractBoundaries.features) {
+      App.data.tractInfo[tract.properties.tractce10] = tract.properties;
+    }
+
+
     // get the maximum population in any census block
     let maxPop = d3.max(censusPopulations, d => d["TOTAL POPULATION"]);
 
     // create a d3 color scale to convert a population number to a color value
     let colorScale = d3.scaleLinear()
       .domain(d3.range(0, 7).map(n => n/6 * maxPop)) // need an array of 6 values to map to the 6 colors
-      .range(['#f7f4f9','#e7e1ef','#d4b9da','#c994c7','#df65b0','#e7298a'])
+      .range(['#7fcdbb','#41b6c4','#1d91c0','#225ea8','#253494','#081d58'])
       .clamp(true);
 
     // filter out census blocks based on the community to focus on englewood
+    blockBoundaries.features = blockBoundaries.features.filter(b => {
+      let tract = App.data.tractInfo[b.properties.tractce10];
 
-    // (this is filtering out too many census blocks, though, including ones in
-    // englewood, need to investigate how to better identify the community area from block#)
-    censusBoundaries.features = censusBoundaries.features.filter(b => {
-      let comm = Math.floor(parseInt(b.properties.tractce10) / 10000);
+      if (tract) {
+        let comm = parseInt(tract.commarea);
 
-      return comm === 67 || comm === 68;
+        return comm === 67 || comm === 68;
+      }
+
+      return false;
     });
 
     // filter out community areas to only draw a border on englewood
@@ -83,10 +95,12 @@ var App = App || {};
 
     // create map elements from the census block boundaries
     // in the style property, color by the color scale based on the census block#
-    L.geoJSON(censusBoundaries, {
+    L.geoJSON(blockBoundaries, {
         style: function (feature) {
             return {
-              color: colorScale(App.data.populations[feature.properties.tract_bloc])
+              color: colorScale(App.data.populations[feature.properties.tract_bloc]),
+              fillOpacity: 0.5,
+              stroke: false
             };
         }
     }).addTo(App.map);
@@ -105,7 +119,8 @@ var App = App || {};
           riseOnHover: true // moves the marker to the front on mouseover
         }
       ).bindPopup(function(layer) { // allow for the popup on click with the name of the location
-        return loc["Organization Name"];
+        return "<strong>" + loc["Organization Name"] + "</strong><br>"
+         + loc["Description of Services Provided in Englewood"];
       }).addTo(App.map);
     }
 
