a5923ef9e576b30ca39e4adcb0273fe9552993d0
Lindmae <ilindm2@uic.edu>
2017-05-24 16:24:15 -0500

    Updated serviceListView to display ~real~ data entries

--- src/js/views/serviceListView.js
+++ src/js/views/serviceListView.js
@@ -14,7 +14,21 @@ let ServiceListView = function(listID) {
   }
 
   function populateList(englewoodLocations) {
-    console.log(englewoodLocations);
+
+    //remove previous entries
+    self.serviceList.selectAll(".serviceEntry").remove();
+
+    //add new entries
+    self.serviceList.selectAll(".serviceEntry")
+    .data(englewoodLocations)
+    .enter()
+    .append("div").attr("class", "serviceEntry")
+    .each(function(d){
+      var current = d3.select(this);
+      current.append("h3").text(function(d) { return d["Organization Name"]; })
+      current.append("h4").text(function(d) { return d.Address; })
+      current.append("p").text(function(d) { return d["Description of Services Provided in Englewood"]; })
+    });
 
   }
 
