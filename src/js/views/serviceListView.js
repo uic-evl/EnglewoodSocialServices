7d2cd7715e4b32444080c589f654caaf7cc1938d
Andrew Burks <andrewtburks@gmail.com>
2017-05-25 09:26:51 -0500

    Updated service list to be bootstrap panels (from normal divs)

--- src/js/views/serviceListView.js
+++ src/js/views/serviceListView.js
@@ -22,12 +22,37 @@ let ServiceListView = function(listID) {
     self.serviceList.selectAll(".serviceEntry")
     .data(englewoodLocations)
     .enter()
-    .append("div").attr("class", "serviceEntry")
+    .append("div").attr("class", "panel panel-info serviceEntry")
     .each(function(d){
-      var current = d3.select(this);
-      current.append("h3").text(function(d) { return d["Organization Name"]; })
-      current.append("h4").text(function(d) { return d.Address; })
-      current.append("p").text(function(d) { return d["Description of Services Provided in Englewood"]; })
+      let panel = d3.select(this);
+      
+      // create heading
+      let panelHeading = panel.append("div")
+        .attr("class", "panel-heading");
+
+      // create body
+      let panelBody = panel.append("div")
+        .attr("class", "panel-body");
+
+      // create footer
+      let panelFooter = panel.append("div")
+        .attr("class", "panel-footer");
+
+      // add organization name to heading
+      panelHeading.append("h4").text(function(d) { return d["Organization Name"]; });
+
+      // add description to body
+      panelBody.append("p").text(function(d) { return d["Description of Services Provided in Englewood"]; });
+
+      // add link to address in footer
+      panelFooter.append("a")
+        .attr("href", "http://maps.google.com/?q=" + d["Address"])
+        .attr("target", "_blank")
+        .html(function(d) {
+          return "<span class='glyphicon glyphicon-share-alt'></span> " +
+          d["Address"];
+        });
+
     });
 
   }
