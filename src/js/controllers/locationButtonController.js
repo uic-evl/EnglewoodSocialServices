9d4cb931e9f9a2e94d4d589a6e6ee2cb0ea1fff9
Lindmae <ilindm2@uic.edu>
2017-05-25 14:18:03 -0500

    Added ability to type location

--- src/js/controllers/locationButtonController.js
+++ src/js/controllers/locationButtonController.js
@@ -14,13 +14,14 @@ let LocationButtonController = function() {
 
   function attachLocationButton(id) {
     self.locationButton = d3.select(id)
-      .on('click', updateLocationOnMap);
+    .on('click', updateLocationOnMap);
     console.log('adding event handler to ', id);
 
   }
 
   function attachAddressLookupButton(id) {
-
+    self.addressLookupButton = d3.select(id)
+    .on('click', getLatLngFromAddress)
     console.log('adding event handler to ', id);
   }
 
@@ -28,30 +29,38 @@ let LocationButtonController = function() {
     // make this and call App.views.map.setLocation()
     // using data from https://www.w3schools.com/html/html5_geolocation.asp
     if (navigator.geolocation) {
-      console.log('found something!')
       navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            }
-          console.log("Latitude: " + pos.lat + "   Longitude: " + pos.lng);
-          App.views.map.jumpToLocation(pos);
-        });
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        App.views.map.jumpToLocation(pos);
+      });
 
-    }
-    else {
-      console.log('wait shit')
     }
   }
 
-  function getLatLngFromAddress(address) {
+  function getLatLngFromAddress() {
     // Google Maps Geocoding API:
     //    https://developers.google.com/maps/documentation/geocoding/start#get-a-key
     // API Key: AIzaSyAUDFjBPoiSQprcBvEhc9w6SJeR3EK4IGI
+
+    console.log(d3.select('#addressInput').node().value);  
+
+    var address = d3.select('#addressInput').node().value;
+
+    var replaced = address.split(' ').join('+');
+    console.log(replaced);
+
+    var object = d3.json("https://maps.googleapis.com/maps/api/geocode/json?address="+replaced+"&key=AIzaSyAUDFjBPoiSQprcBvEhc9w6SJeR3EK4IGI", function(err,d){
+    console.log(d.results[0].geometry.location);
+    App.views.map.jumpToLocation(d.results[0].geometry.location);
+    })
+
   }
 
-  return {
-    attachLocationButton,
-    attachAddressLookupButton
-  };
+return {
+  attachLocationButton,
+  attachAddressLookupButton
+};
 };
