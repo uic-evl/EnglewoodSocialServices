104de43b7289cca507c8bda5053823ae4e0e0f8a
Lindmae <ilindm2@uic.edu>
2017-05-25 12:47:20 -0500

    Updated locationButtonController to receive current lat and long

--- src/js/controllers/locationButtonController.js
+++ src/js/controllers/locationButtonController.js
@@ -7,13 +7,35 @@ let LocationButtonController = function() {
     locationButton: null
   };
 
+  init();
+
+  function init(){}
+
   function attachLocationButton(id) {
+    self.allServicesButton = d3.select(id)
+      .on('click', updateLocationOnMap);
     console.log('adding event handler to ', id);
+
   }
 
   function updateLocationOnMap() {
     // make this and call App.views.map.setLocation()
     // using data from https://www.w3schools.com/html/html5_geolocation.asp
+    if (navigator.geolocation) {
+      console.log('found something!')
+      navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+          console.log("Latitude: " + pos.lat + "   Longitude: " + pos.lng); 
+          // App.views.map.setView([pos.lat, pos.lng], 15);
+        });
+      
+    }
+    else {
+      console.log('wait shit')
+    }
   }
 
   return {
