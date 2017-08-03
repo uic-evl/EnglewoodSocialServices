387939176d7938f05458c3e166ff9b4544f5e708
Lindmae <ilindm2@uic.edu>
2017-08-03 14:53:41 -0500

    Must have one option clicked for landing page

--- src/js/controllers/modalController.js
+++ src/js/controllers/modalController.js
@@ -19,6 +19,7 @@ let modalController = function() {
 
     self.acceptButton = self.modal.select("#modalAcceptButton")
       .on('click', acceptButtonClicked);
+      
 
     self.orgSearchInput = d3.selectAll("#modalSearchInput")
       .on("input", onInput)
@@ -27,18 +28,16 @@ let modalController = function() {
           console.log("enter!");
           // hitting enter in the input is equivalent to pressing accept button
           acceptButtonClicked();
-          $('#landing-page').modal('hide');
-
         }
       });
 
     self.addressInput = d3.select('#modalAddressInput')
+      .on("input", changeButtonState)
       .on("keyup", function() {
         if (d3.event.keyCode == 13) {
           console.log("enter!");
           // hitting enter in the input is equivalent to pressing accept button
           acceptButtonClicked();
-          $('#landing-page').modal('hide');
         }
       });
 
@@ -55,6 +54,8 @@ let modalController = function() {
     let searchData = App.models.socialServices.getSearchedData(searchTerm);
 
     countChanged(searchData);
+
+    changeButtonState();
   }
 
   function countChanged(data) {
@@ -71,17 +72,25 @@ let modalController = function() {
     }
   }
 
-  function acceptButtonClicked(){
+  function changeButtonState(){
+    let orgSearchText = self.orgSearchInput.node().value;
+
+    let searchTerm = _.lowerCase(orgSearchText);
+
+    let searchData = App.models.socialServices.getSearchedData(searchTerm);
 
     var address = self.addressInput.node().value;
-    if(address.length !== 0){
-      App.controllers.locationButton.getLatLngFromAddress(address);
+
+    var service = document.getElementById("currentServiceSelection").innerHTML;
+    if(searchTerm.length != 0 || address.length != 0 || !service.includes("Select Services...")){
+      $('#modalAcceptButton').removeClass("disabled");
     }
-    else if(address.length ==0){
-      // self.map.setView([41.779786, -87.644778], 15);
-      // console.log(App.views.map);
-      App.views.map.clearLocation();
+    else{
+      $('#modalAcceptButton').addClass("disabled");
     }
+  }
+
+  function acceptButtonClicked(){
 
     let orgSearchText = self.orgSearchInput.node().value;
 
@@ -89,11 +98,31 @@ let modalController = function() {
 
     let searchData = App.models.socialServices.getSearchedData(searchTerm);
 
-    App.views.map.updateServicesWithFilter(searchData);
-    App.views.serviceList.populateList(searchData);
+    var address = self.addressInput.node().value;
 
-    // d3.select("#searchInput").node().value = orgSearchText;
-    App.controllers.search.countChanged(searchData);
+    var service = document.getElementById("currentServiceSelection").innerHTML;
+    //make sure at least one option is chosen
+    console.log(service);
+    if(searchTerm.length == 0 && address.length == 0 && service.includes("Select Services...")){
+      console.log("its empty!!");
+    }
+    else{
+      if(address.length !== 0){
+        App.controllers.locationButton.getLatLngFromAddress(address);
+      }
+      else if(address.length ==0 ){
+        App.views.map.clearLocation();
+      }
+
+      App.views.map.updateServicesWithFilter(searchData);
+      App.views.serviceList.populateList(searchData);
+
+      App.controllers.search.countChanged(searchData);
+      $('#landing-page').modal('hide');
+
+      }
+    
+    
 
 
   }
