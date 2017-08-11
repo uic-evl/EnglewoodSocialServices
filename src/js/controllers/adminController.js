e36f2586a4e88ddc4b8dce35c72cacc299914ae3
Andrew Burks <andrewtburks@gmail.com>
2017-08-11 15:04:13 -0500

    Get rid of notes at end of admin controller file

--- src/js/controllers/adminController.js
+++ src/js/controllers/adminController.js
@@ -142,33 +142,3 @@ function restoreFile(d){
   xhr.send(JSON.stringify(d));
   location.reload();
 }
-
-/*
-To send request to change used file
-
-xhr = new XMLHttpRequest();
-xhr.open("PUT", "/admin/chooseold");
-xhr.setRequestHeader("Content-type", "application/json");
-
-xhr.onload = function () {
-  // LOG file received in this.responseText -- populate log
-  console.log(this.responseText);
-};
-xhr.onerror = function (e) {
-  reject(e);
-};
-
-xhr.send(JSON.stringify({
-  "time": 1502470585985,
-  "name": "EnglewoodLocations.csv"
-}));
-
-d3.select(--button--).datum({
-  "time": 1502470585985,
-  "name": "EnglewoodLocations.csv"
-}).on("click", restoreFile)
-
-function restoreFile(d) {
-
-}
-*/
