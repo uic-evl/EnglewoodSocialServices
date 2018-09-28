0cc764457d60487d51cd62bf82d3a81b8c065fdd
Joshua Castor <jcasto3@uic.edu>
2018-09-28 15:36:22 -0500

    Allow CORS on the server

--- server.js
+++ server.js
@@ -27,6 +27,13 @@ var basic = auth.basic({
 var app = express();
 let admin = express();
 
+// enable cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 admin.use(bodyParser.json());
 admin.use(auth.connect(basic));
 
